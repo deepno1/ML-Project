bfc2b4f6c307fee2be1e2d750bb2ba0b242705e7
Deep Malusare <deepmalusare43@gmail.com>
2025-07-19 03:07:43 +0530

    Add logger

---
+++ src/mlproject/logger.py
@@ -0,0 +1,15 @@
+import logging
+from datetime import datetime
+import os
+
+LOG_FILE = "{}.log".format(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
+log_path = os.path.join(os.getcwd(),"logs")
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format= "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO
+)
